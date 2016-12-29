83253f527523ec790ef3f5394de12ac00f76ee6d
Ximidar <Ximidar@gmail.com>
2016-12-29 23:53:34 +0000

    Initial Commit

---
+++ octoprint_Meta_Reader/static/js/Meta_Reader.js
@@ -0,0 +1,28 @@
+/*
+ * View model for OctoPrint-Meta_reader
+ *
+ * Author: Matt Pedler
+ * License: AGPLv3
+ */
+$(function() {
+    function Meta_readerViewModel(parameters) {
+        var self = this;
+
+        // assign the injected parameters, e.g.:
+        // self.loginStateViewModel = parameters[0];
+        // self.settingsViewModel = parameters[1];
+
+        // TODO: Implement your plugin's view model here.
+    }
+
+    // view model class, parameters for constructor, container to bind to
+    OCTOPRINT_VIEWMODELS.push([
+        Meta_readerViewModel,
+
+        // e.g. loginStateViewModel, settingsViewModel, ...
+        [ /* "loginStateViewModel", "settingsViewModel" */ ],
+
+        // e.g. #settings_plugin_Meta_Reader, #tab_plugin_Meta_Reader, ...
+        [ /* ... */ ]
+    ]);
+});
