a689ddc585554f3c37f9c8d1e8f4dd4f1d50399a
Ximidar <Ximidar@gmail.com>
2016-12-30 16:48:12 +0000

    changed Class name

--- Meta_Reader/__init__.py
+++ Meta_Reader/__init__.py
@@ -4,7 +4,7 @@ from File_Reader import File_Reader
 import octoprint.plugin
 from threading import Timer
 
-class Meta_readerPlugin(octoprint.plugin.SettingsPlugin,
+class Meta_reader(octoprint.plugin.SettingsPlugin,
                         octoprint.plugin.AssetPlugin,
                         octoprint.plugin.TemplatePlugin,
                         octoprint.plugin.StartupPlugin,
@@ -85,11 +85,11 @@ class Meta_readerPlugin(octoprint.plugin.SettingsPlugin,
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "Meta_reader Plugin"
+__plugin_name__ = "Meta_reader"
 
 def __plugin_load__():
     global __plugin_implementation__
-    __plugin_implementation__ = Meta_readerPlugin()
+    __plugin_implementation__ = Meta_reader()
 
     global __plugin_hooks__
     __plugin_hooks__ = {
